59e39a260b0d6392e828d91e8b8bd2dd1a51d17d
RodrigoMASRamos <rodrigo.msramos1@gmail.com>
2020-08-28 10:22:18 -0300

    Esboço da resolução dos ex 46 e 56

--- PycharmProjects/PythonExercicios/ex046.py
+++ PycharmProjects/PythonExercicios/ex046.py
@@ -2,3 +2,6 @@
 #
 # Faça um programa que mostre na tela uma CONTAGEM REGRESSIVA para o estouro de fogos de artifício, indo de 10 até 0,
 # com uma pausa de 1 segundo entre eles.
+
+for c in range(0,11,-1):
+    print(c)
--- PycharmProjects/PythonExercicios/ex056.py
+++ PycharmProjects/PythonExercicios/ex056.py
@@ -4,3 +4,5 @@
 #   - A MÉDIA DE IDADE do grupo.              - Quantas mulheres têm MENOS DE 20 anos.
 #   - Qual é o nome do homem MAIS VELHO
 
+for c in range(1,5):
+    print(c)
